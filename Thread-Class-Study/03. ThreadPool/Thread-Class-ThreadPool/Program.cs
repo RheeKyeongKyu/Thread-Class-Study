25f5f01be3c5bdbb5dc1ce5e64e1a9fa1e05033d
Skywalker <rhee469@naver.com>
2022-01-14 22:27:13 +0900

    Execute Method by using a Thread in the ThreadPool

---
+++ Thread-Class-Study/03. ThreadPool/Thread-Class-ThreadPool/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+
+namespace Thread_Class_ThreadPool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Excutes Calc() Method by using a Thread in the ThreadPool
+            // used when there is no return value
+            // Calc() Method will be executed when ThreadPool thread becomes available
+            ThreadPool.QueueUserWorkItem(callBack: Calc);               // radius = null
+            ThreadPool.QueueUserWorkItem(callBack: Calc, state: 10.0);  // radius = 10
+            ThreadPool.QueueUserWorkItem(callBack: Calc, state: 20.0);  // radius = 20
+
+            // Wait for user input
+            Console.ReadLine();
+        }
+
+        private static void Calc(object radius)
+        {
+            if (radius == null)
+                return;
+
+            double r = (double)radius;
+            double area = r * r * 3.14;
+            Console.WriteLine($"r={r}, area={area}");
+        }
+    }
+}
