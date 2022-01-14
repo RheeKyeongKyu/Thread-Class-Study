a0d11d263111b888b6f774e32cc3db9546162b9d
Skywalker <rhee469@naver.com>
2022-01-14 18:23:57 +0900

    - Passing parameter to Thread - Foreground Thread and Background Thread

---
+++ Thread-Class-Study/02. Thread Class/Thread-Class-BackgroundForeground/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Threading;
+
+namespace Thread_Class_BackgroundForeground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Foreground, Background Thread";
+            Console.WriteLine("********** Foreground Vs Background **********");
+            Console.WriteLine();
+            Console.Write("Enter 1 to Start Foreground Thread and 2 to Start Background Thread (other to Exit): ");
+                
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            switch (number)
+            {
+                case 1:
+                    StartForegroundThread();
+                    break;
+                case 2:
+                    StartBackgroundThread();
+                    break;
+                default:
+                    return;
+            }
+
+            // Main Thread
+            Console.WriteLine("Main() method completed...");
+        }
+
+
+        private static void StartForegroundThread()
+        {
+            // Foreground Thread (default)
+            // Keeps running even after main thread terminates
+            Thread thread1 = new Thread(new ParameterizedThreadStart(Run));
+            thread1.Start("Foreground Thread");
+        }
+
+        private static void StartBackgroundThread()
+        {
+            // Background Thread
+            // Ends when main thread terminates
+            Thread thread2 = new Thread(new ParameterizedThreadStart(Run));
+            thread2.IsBackground = true;
+            thread2.Start("Background Thread");
+        }
+
+        private static void Run(object threadType)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Thread Type: {threadType}, {i}");
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
---
+++ Thread-Class-Study/02. Thread Class/Thread-Class-Parameter/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Threading;
+
+namespace Thread_Class_Parameter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Using ThreadStart without parameters
+            Thread thread1 = new Thread(new ThreadStart(Run));
+            thread1.Start();
+
+            // Passing parameter to ParameterizedThreadStart
+            // Passing radius as parameter of Start()
+            Thread thread2 = new Thread(start: new ParameterizedThreadStart(Calc));
+            thread2.Start(10.00);
+
+            // Passing parameter in ThreadStart
+            Thread thread3 = new Thread(() => Sum(10, 20, 30));
+            thread3.Start();
+
+            // Passing array of object as parameter to ParameterizedThreadStart
+            Thread thread4 = new Thread(start: new ParameterizedThreadStart(Multiplication));
+            thread4.Start(new object[5] {1, 3, 5, 7, 9});
+        }
+
+
+        private static void Run()
+        {
+            Console.WriteLine("Run");
+        }
+
+        // Accept radius parameter as object type
+        private static void Calc(object radius)
+        {
+            double r = (double)radius;
+            double area = r * r * 3.14;
+            Console.WriteLine($"r={r}, area={area}");
+        }
+
+        private static void Sum(int d1, int d2, int d3)
+        {
+            int sum = d1 + d2 + d3;
+            Console.WriteLine($"Sum: {sum}");
+        }
+
+        private static void Multiplication(object numbers)
+        {
+            Array nums = (Array)numbers;
+
+            int result = 1;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                result *= (int)nums.GetValue(i);
+            }
+
+            Console.WriteLine($"Multiplication: {result}");
+        }
+    }
+}
