fdf80993e08b1bb04f8be947a09ec6824f6d4494
Skywalker <rhee469@naver.com>
2022-01-15 23:04:40 +0900

    BackgroundWorker example with Winform UI Control

---
+++ Thread-Class-Study/05. BackgroundWorker/Thread-Class-BackgroundWorker-Winform/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Thread_Class_BackgroundWorker_Winform
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRun = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(12, 12);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(77, 23);
+            this.btnRun.TabIndex = 0;
+            this.btnRun.Text = "Run";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Enabled = false;
+            this.btnCancel.Location = new System.Drawing.Point(95, 12);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(77, 23);
+            this.btnCancel.TabIndex = 1;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(12, 41);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(424, 21);
+            this.textBox1.TabIndex = 2;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(12, 68);
+            this.progressBar1.Minimum = 1;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(424, 23);
+            this.progressBar1.Step = 1;
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 3;
+            this.progressBar1.Value = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(448, 102);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnRun);
+            this.Name = "Form1";
+            this.Text = "BackgroundWorker Sample";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ Thread-Class-Study/05. BackgroundWorker/Thread-Class-BackgroundWorker-Winform/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+using System.Threading;
+using System.Diagnostics;
+
+namespace Thread_Class_BackgroundWorker_Winform
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private BackgroundWorker worker;
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            // Reset UI
+            progressBar1.Value = 1;
+            btnRun.Enabled = false;
+            btnCancel.Enabled = true;
+
+            // Set progress report and cancel property to true
+            worker = new BackgroundWorker();
+            worker.WorkerReportsProgress = true;
+            worker.WorkerSupportsCancellation = true;
+
+            // Assign event handler
+
+            // DoWork event handler cannot access UI control
+            worker.DoWork += new DoWorkEventHandler(Worker_DoWork);
+
+            // ProgressChanged, RunWorkerCompleted event handler can access UI control (they both run in the UI Thread)
+            worker.ProgressChanged += new ProgressChangedEventHandler(Worker_ProgressChanged);
+            worker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(Worker_RunWorkerCompleted);
+
+            // Start Worker Thread
+            worker.RunWorkerAsync();
+        }
+
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            // Reset UI
+            btnRun.Enabled = true;
+            btnCancel.Enabled = false;
+            
+            // Request cancellation of Worker Thread
+            worker.CancelAsync();
+        }
+
+        private void Worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            // Upload 100 items
+            int items = 100;
+            for (int i = 1; i <= items; i++)
+            {
+                // Check if there are any request for cancellation of Worker Thread
+                if (worker.CancellationPending)
+                {
+                    // Cancel running task using by setting 'e.Cancel' to true and return
+                    e.Cancel = true;
+                    return;
+                }
+
+                // Do upload task
+                string fileName = $"Data_{i}.txt";
+                Upload(fileName);
+
+                // Report progress
+                worker.ReportProgress(percentProgress: i, userState: fileName);
+            }
+
+            // Set the result of asynchronous task
+            e.Result = items;
+        }
+
+        void Upload(string fileName)
+        {
+            Debug.WriteLine($"Uploading {fileName}");
+            Thread.Sleep(100);
+        }
+
+        private void Worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            // Update UI Control
+            // Control.Invoke() not needed
+            textBox1.Text = $"Progress : {e.ProgressPercentage}";
+            progressBar1.PerformStep();
+        }
+        
+        private void Worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            // Update UI Control
+            // Control.Invoke() not needed
+            if (e.Cancelled)
+            {
+                textBox1.Text = "Task Cancelled";
+            }
+            else if (e.Error != null)
+            {
+                throw e.Error;
+            }
+            else
+            {
+                textBox1.Text = $"{e.Result} files uploaded";
+            }
+        }
+    }
+}
