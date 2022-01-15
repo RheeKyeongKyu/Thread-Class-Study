60bd4a658b7cdbabbdfa021e8d2213d290464c8b
Skywalker <rhee469@naver.com>
2022-01-15 14:16:32 +0900

    Execute method by Worker Thread (BackgroundWorker Class)

---
+++ Thread-Class-Study/05. BackgroundWorker/Thread-Class-BackgroundWorker-Console/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Thread_Class_BackgroundWorker_Console
+{
+    using System;
+    using System.ComponentModel;
+
+    class Program
+    {
+        private BackgroundWorker worker;
+
+        static void Main(string[] args)
+        {
+            new Program().Execute();
+            Console.Read();
+        }
+
+        public void Execute()
+        {
+            //// Starting a Worker Thread from ThreadPool
+
+            // Define BackgroundWorker Instance
+            worker = new BackgroundWorker();
+
+            // Specifying actual work to be done through DoWorkEventHandler
+            worker.DoWork += new DoWorkEventHandler(worker_DoWork);
+
+            // Starting a task by callinng the RunWorkerAsync() method
+            worker.RunWorkerAsync();
+        }
+
+        // Task method executed by Worker Thread
+        private void worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            // Time consuming task
+            Console.WriteLine("Long running task");
+        }
+    }
+}
