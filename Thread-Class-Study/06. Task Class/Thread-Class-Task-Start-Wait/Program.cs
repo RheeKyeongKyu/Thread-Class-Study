174d6acf6dff7eeb5535282ad5514df13ce645da
Skywalker <rhee469@naver.com>
2022-01-16 18:38:22 +0900

    Create and start Thread with Task Instance

---
+++ Thread-Class-Study/06. Task Class/Thread-Class-Task-Start-Wait/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Thread_Class_Task_Start_Wait
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //// Creating Task Instance
+
+            // Method 1. Creating Task Instance by assigning Run method to Task constructor 
+            Task t1 = new Task(new Action(Run));
+
+            // Method 2. Creating Task Instance by lambda expression
+            Task t2 = new Task(() =>
+            {
+                Console.WriteLine("Long query");
+            });
+
+            // Start Task Thread
+            t1.Start();
+            t2.Start();
+
+            // Wait for Task to end
+            t1.Wait();
+            t2.Wait();
+        }
+
+        private static void Run()
+        {
+            Console.WriteLine("Long running method");
+        }
+    }
+}
