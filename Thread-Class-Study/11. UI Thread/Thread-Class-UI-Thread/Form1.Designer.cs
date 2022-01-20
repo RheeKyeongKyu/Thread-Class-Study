01b99e7517602d577dc825c89beca9178182828d
Skywalker <rhee469@naver.com>
2022-01-20 17:36:21 +0900

    UI Program with UI Thread and Worker Thread

---
+++ Thread-Class-Study/11. UI Thread/Thread-Class-UI-Thread/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Thread_Class_UI_Thread
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnQuery = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnQuery
+            // 
+            this.btnQuery.Location = new System.Drawing.Point(12, 12);
+            this.btnQuery.Name = "btnQuery";
+            this.btnQuery.Size = new System.Drawing.Size(75, 23);
+            this.btnQuery.TabIndex = 0;
+            this.btnQuery.Text = "Query";
+            this.btnQuery.UseVisualStyleBackColor = true;
+            this.btnQuery.Click += new System.EventHandler(this.btnQuery_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 41);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(275, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Enabled = false;
+            this.btnCancel.Location = new System.Drawing.Point(93, 12);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(299, 73);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnQuery);
+            this.Name = "Form1";
+            this.Text = "UI Thread";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnQuery;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
+
---
+++ Thread-Class-Study/11. UI Thread/Thread-Class-UI-Thread/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Thread_Class_UI_Thread
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // UI Thread: Thread that creates UI Control and owns the Window Handle of the Control
+        // Worker Thread: Thread that does not own the UI Control
+
+        // Normally, a UI Program has one UI Thread and multiple Worker Thread (But Possible to have multiple UI Thread)
+
+        Thread worker;
+
+        private void btnQuery_Click(object sender, EventArgs e)
+        {
+            // Update UI Control by UI Thread
+            btnQuery.Enabled = false;
+            btnCancel.Enabled = true;
+            textBox1.Text = string.Empty;
+
+            // Start Worker Thread
+            worker = new Thread(Run);
+            worker.Start();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            // Abort Worker Thread
+            worker.Abort();
+
+            // Update UI Control by UI Thread
+            btnQuery.Enabled = true;
+            btnCancel.Enabled = false;
+
+            // Refresh TextBox by UI Thread
+            UpdateTextBox("Query Cancelled");
+        }
+
+        private void Run()
+        {
+            // Long running DB query
+            Thread.Sleep(5000);
+
+            // Refresh TextBox by Worker Thread
+            UpdateTextBox("Query Result");
+        }
+
+        private void UpdateTextBox(string data)
+        {
+            // Check if calling Thread is Worker Thread
+            if (textBox1.InvokeRequired)
+            {
+                // If it is Worker Thread
+                textBox1.BeginInvoke(new Action(() =>
+                {
+                    textBox1.Text = data;
+                    btnQuery.Enabled = true;
+                    btnCancel.Enabled = false;
+                }));
+            }
+            else
+            {
+                // If it is UI Thread
+                textBox1.Text = data;
+                btnQuery.Enabled = true;
+                btnCancel.Enabled = false;
+            }
+        }
+    }
+}
